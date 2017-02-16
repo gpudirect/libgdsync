4a76417342b5225928790cc631039583abe86fb5
Davide Rossetti <davide.rossetti@gmail.com>
2017-02-15 16:06:30 -0800

    add gds_ prefix to low-level funs, add basic POWER support

--- tests/test_utils.h
+++ tests/test_utils.h
@@ -15,10 +15,6 @@ static inline void prof_disable(struct prof *p) {}
 static inline void prof_reset(struct prof *p) {}
 #endif
 
-#ifndef ACCESS_ONCE
-#define ACCESS_ONCE(x) (*(volatile typeof(x) *)&(x))
-#endif
-
 typedef int64_t us_t;
 static inline us_t gds_get_time_us()
 {
@@ -31,10 +27,29 @@ static inline us_t gds_get_time_us()
         return (us_t)ts.tv_sec * 1000 * 1000 + (us_t)ts.tv_nsec / 1000;
 }
 
+#if defined(__x86_64__) || defined (__i386__)
+
 static inline void gds_cpu_relax(void)
 {
         asm volatile("pause\n": : :"memory");
 }
+
+#define gds_wmb()   asm volatile("sfence" ::: "memory")
+
+#elif defined(__powerpc__)
+static void gds_cpu_relax(void) __attribute__((unused)) ;
+static void gds_cpu_relax(void)
+{
+}
+
+static void gds_wmb(void) __attribute__((unused)) ;
+static void gds_wmb(void) 
+{
+	asm volatile("sync") ; 
+}
+#else
+#error "platform not supported"
+#endif
 
 static inline void gds_busy_wait_us(us_t tmout)
 {
@@ -45,10 +60,12 @@ static inline void gds_busy_wait_us(us_t tmout)
         } while ((tm-gds_get_time_us()) > 0);
 }
 
-#define wmb()   asm volatile("sfence" ::: "memory")
+#ifndef ACCESS_ONCE
+#define ACCESS_ONCE(x) (*(volatile typeof(x) *)&(x))
+#endif
 
 void gds_atomic_set_dword(uint32_t *ptr, uint32_t value)
 {
         ACCESS_ONCE(*ptr) = value;
-        wmb();
+        gds_wmb();
 }
