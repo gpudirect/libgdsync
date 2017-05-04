3c3c209ee77b9b36387ff19b0538d1cc3edb0f70
e-ago <elena.ago@gmail.com>
2017-05-04 12:10:51 -0700

    gds_stream_queue_send_ex + gds_rollback_qp

--- src/apis.cpp
+++ src/apis.cpp
@@ -210,7 +210,9 @@ int gds_stream_queue_send_ex(CUstream stream, struct gds_qp *qp, struct ibv_exp_
 
         ret = gds_post_pokes(stream, 1, &send_info, dw, val);
         if (ret) {
-                goto out;
+            //the request has been committed here
+            gds_rollback_qp(qp, &send_info, IBV_EXP_ROLLBACK_ABORT_LATE);
+            goto out;
         }
 out:
         return ret;
