6ead1cfe68ed2e030cb56fd0e239d7c7dff688fb
e-ago <elena.ago@gmail.com>
2018-04-12 13:47:46 +0200

    Initial commit: add param_flush in case of need_flush == true

--- src/gdsync.cpp
+++ src/gdsync.cpp
@@ -409,6 +409,9 @@ out:
 //-----------------------------------------------------------------------------
 static bool hotfix_print=false;
 
+static bool fwait_gmem_done=false;
+static CUdeviceptr fwait_gmem;
+
 static int gds_fill_poll(gds_op_list_t &ops, CUdeviceptr ptr, uint32_t magic, int cond_flag, int flags)
 {
         int retcode = 0;
@@ -420,6 +423,7 @@ static int gds_fill_poll(gds_op_list_t &ops, CUdeviceptr ptr, uint32_t magic, in
 
         bool need_flush = (flags & GDS_WAIT_POST_FLUSH) ? true : false;
 
+#if 0
 //Flag CU_STREAM_WAIT_VALUE_FLUSH generates an error with CUDA 9.x
 #if defined(__x86_64__) || defined (__i386__) // || defined (__ppc64le__)
         need_flush=false;
@@ -429,7 +433,7 @@ static int gds_fill_poll(gds_op_list_t &ops, CUdeviceptr ptr, uint32_t magic, in
             hotfix_print=true;
         }
 #endif
-
+#endif
         CUstreamBatchMemOpParams param;
         param.operation = CU_STREAM_MEM_OP_WAIT_VALUE_32;
         param.waitValue.address = dev_ptr;
@@ -452,15 +456,44 @@ static int gds_fill_poll(gds_op_list_t &ops, CUdeviceptr ptr, uint32_t magic, in
                 retcode = EINVAL;
                 goto out;
         }
+#if 0        
         if (need_flush)
                 param.waitValue.flags |= CU_STREAM_WAIT_VALUE_FLUSH;
+#endif
         gds_dbg("op=%d addr=%p value=%08x cond=%s flags=%08x\n",
                 param.operation,
                 (void*)param.waitValue.address,
                 param.waitValue.value,
                 cond_str,
                 param.waitValue.flags);
+
         ops.push_back(param);
+
+        //Fake wait value on 0 gmemory area
+        if(need_flush)
+        {
+            CUstreamBatchMemOpParams param_flush;
+            if(!fwait_gmem_done)
+            {
+                CUCHECK(cuMemAlloc(&fwait_gmem, 1*sizeof(char)));
+                CUCHECK(cuMemsetD8(fwait_gmem, 0, 1));
+                fwait_gmem_done=true;
+            }    
+
+            param_flush.operation = CU_STREAM_MEM_OP_WAIT_VALUE_32;
+            param_flush.waitValue.address = fwait_gmem;
+            param_flush.waitValue.value = 0;
+            param_flush.waitValue.flags = CU_STREAM_WAIT_VALUE_EQ;
+
+            ops.push_back(param_flush);
+
+            gds_dbg("need_flush=%d, fwait_gmem_done=%d, op=%d addr=%p value=%08x flags=%08x\n",
+                need_flush, fwait_gmem_done,
+                param_flush.operation,
+                (void*)param_flush.waitValue.address,
+                param_flush.waitValue.value,
+                param_flush.waitValue.flags);
+        }
 out:
         return retcode;
 }
