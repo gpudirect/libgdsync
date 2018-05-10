ac20a78f22b37d0560a28b66ee27796eaed61bec
Davide Rossetti <davide.rossetti@gmail.com>
2018-05-09 19:32:37 -0700

    use CUDATK if set

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@
 cd build
 echo "PREFIX=$PREFIX"
 echo "CUDADRV=$CUDADRV"
+echo "CUDATK=$CUDATK"
 echo "CUDA=$CUDA"
 echo "MPI_HOME=$MPI_HOME"
 
@@ -16,16 +17,23 @@ if [ ! -e Makefile ]; then
     echo "configuring..."
     EXTRA=
     if [ "x$CUDADRV" != "x" ]; then
-        EXTRA="$EXTRA --with-cuda-driver=${CUDADRV}"
+        EXTRA+=" --with-cuda-driver=${CUDADRV}"
     fi
-    EXTRA="$EXTRA --enable-test"
-    EXTRA="$EXTRA --enable-extended-memops"
+    if [ "x$CUDATK" != "x" ]; then
+        EXTRA+=" --with-cuda-toolkit=$CUDATK"
+    elif [ "x$CUDA" != "x" ]; then
+        EXTRA+=" --with-cuda-toolkit=$CUDA"
+    else
+        echo "ERROR: CUDA toolkit path not passed"
+        exit
+    fi
+    EXTRA+=" --enable-test"
+    EXTRA+=" --enable-extended-memops"
     #EXTRA="$EXTRA --with-gdstools=$PREFIX"
 
     ../configure \
         --prefix=$PREFIX \
         --with-libibverbs=$PREFIX \
-        --with-cuda-toolkit=$CUDA \
         --with-gdrcopy=$PREFIX \
         --with-mpi=$MPI_HOME \
         $EXTRA
