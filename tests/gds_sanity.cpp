791dc200fcbe1efaa354f89c7639018294423a5e
Davide Rossetti <davide.rossetti@gmail.com>
2018-05-30 10:02:43 -0700

    remove unused -s option. add -N NOR test option. remove stream creation (may collide with master branch).

--- tests/gds_sanity.cpp
+++ tests/gds_sanity.cpp
@@ -54,17 +54,16 @@ int main(int argc, char *argv[])
 	int gpu_id = 0;
         int num_iters = 50;
         // this seems to minimize polling time
-        int sleep_us = 10;
 	size_t page_size = sysconf(_SC_PAGESIZE);
 	size_t size = 1024*64;
         int use_gpu_buf = 0;
         int use_flush = 0;
         int use_membar = 0;
-        CUstream gpu_stream;
+        int use_nor = 0;
 
         while(1) {            
                 int c;
-                c = getopt(argc, argv, "d:n:s:hfgm");
+                c = getopt(argc, argv, "d:n:hfgmN");
                 if (c == -1)
                         break;
 
@@ -78,9 +77,6 @@ int main(int argc, char *argv[])
                 case 'n':
                         num_iters = strtol(optarg, NULL, 0);
                         break;
-                case 's':
-                        sleep_us = strtol(optarg, NULL, 0);
-                        break;
                 case 'f':
                         use_flush = 1;
                         printf("INFO enabling flush\n");
@@ -89,13 +85,20 @@ int main(int argc, char *argv[])
                         use_gpu_buf = 1;
                         printf("INFO polling on GPU buffer\n");
                         break;
+                case 'N':
+                        use_nor = 1;
+                        printf("INFO polling using NOR\n");
+                        break;
                 case 'h':
                         printf("Usage:\n"
-                               " %s [-d <gpu ordinal>][-n <iters>][-s <sleep us>][hfgm]\n"
+                               " %s [options]\n"
                                "Options:\n"
+                               " -d id  use gpu ordinal id\n"
+                               " -n n   iterate n times\n"
                                " -f     issue a GPU RDMA flush following each poll\n"
                                " -g     allocate all memory on GPU\n"
                                " -m     issue memory barrier between signal and data stores\n"
+                               " -N     poll memory using NOR condition (requires Volta)\n"
                                " -h     this help\n", argv[0]);
                         exit(EXIT_SUCCESS);
                         break;
@@ -110,13 +113,10 @@ int main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 
-        //CUCHECK(cuStreamCreate(&gpu_stream, 0));
-
         puts("");
         printf("number iterations %d\n", num_iters);
         printf("use poll flush %d\n", use_flush);
         printf("use poke membar %d\n", use_membar);
-        printf("sleep %dus\n", sleep_us);
         printf("buffer size %zd\n", size);
         printf("poll on %s buffer\n", use_gpu_buf?"GPU":"CPU");
         printf("write on %s buffer\n", use_gpu_buf?"GPU":"CPU");
@@ -159,6 +159,7 @@ int main(int argc, char *argv[])
         int round;
         int print_dots = 0;
         for (i = 0, value = 1; i < num_iters; ++i, ++value) {
+                uint32_t bit = 0;
                 for (round = 0; round < 2; ++round) {
                         ASSERT(value <= INT_MAX);
                         uint32_t *h_signal = (uint32_t*)h_buf +  ((0) % (size/sizeof(uint32_t)));
@@ -193,9 +194,18 @@ int main(int argc, char *argv[])
                                 ++k;
 
                                 // wait for CPU signal
-                                // while (d_signal != 0);
                                 descs[k].tag = GDS_TAG_WAIT_VALUE32;
-                                GDSCHECK(gds_prepare_wait_value32(&descs[k].wait32, signal, value, GDS_WAIT_COND_EQ, poll_flags));
+                                if (use_nor) {
+                                        bit = 1U<<(value & 31);
+                                        uint32_t msk = ~bit;
+                                        gpu_dbg("signal=%08x msk=%08x\n", bit, msk);
+                                        gds_atomic_set_dword(h_signal, bit);
+                                        GDSCHECK(gds_prepare_wait_value32(&descs[k].wait32, signal, msk, GDS_WAIT_COND_NOR, poll_flags));
+                                }
+                                else {
+                                        // while (d_signal != 0);
+                                        GDSCHECK(gds_prepare_wait_value32(&descs[k].wait32, signal, value, GDS_WAIT_COND_EQ, poll_flags));
+                                }
                                 ++k;
 
                                 descs[k].tag = GDS_TAG_WRITE_VALUE32;
@@ -242,15 +252,31 @@ int main(int argc, char *argv[])
                                 GDSCHECK(gds_stream_post_descriptors(gpu_stream, k, descs, 0));
                         }
                         else {
-                                ASSERT(ACCESS_ONCE(*h_signal) == (value-1));
-                                ASSERT(ACCESS_ONCE(*h_done) == (value-1));
+                                int retcode;
+                                // verify that the stream is still stuck at the 1st wait value
+                                usleep(100);
+                                ASSERT(cuStreamQuery(gpu_stream) == CUDA_ERROR_NOT_READY);
+                                // verify that memory words still have their expected initial value
+                                if (use_nor) {
+                                        ASSERT(gds_atomic_read_dword(h_signal) & bit);
+                                }
+                                else {
+                                        ASSERT(gds_atomic_read_dword(h_signal) == (value-1));
+                                }
+                                ASSERT(gds_atomic_read_dword(h_done) == (value-1));
                                 
                                 gpu_dbg("%d:       dbg @%p:%08x\n", i, h_dbg, ACCESS_ONCE(*h_dbg));
                                 gpu_dbg("%d:       sig @%p:%08x\n", i, h_signal, ACCESS_ONCE(*h_signal));
                                 gpu_dbg("%d:      done @%p:%08x\n", i, h_done, ACCESS_ONCE(*h_done));
-                                gpu_dbg("%d: write sig @%p=%08x\n", i, h_signal, value);
-                                gds_atomic_set_dword(h_signal, value);
-                                gds_wmb();
+                                if (use_nor) {
+                                        // unset the bit
+                                        gpu_dbg("%d: write sig @%p=%08x\n", i, h_signal, ~bit);
+                                        gds_atomic_set_dword(h_signal, ~bit);
+                                }
+                                else {
+                                        gpu_dbg("%d: write sig @%p=%08x\n", i, h_signal, value);
+                                        gds_atomic_set_dword(h_signal, value);
+                                }
                                 gpu_dbg("%d:       sig @%p:%08x\n", i, h_signal, ACCESS_ONCE(*h_signal));
                                 // enough for the GPU to wake up and observe the updated values in the prints below
                                 //usleep(100);
@@ -258,19 +284,18 @@ int main(int argc, char *argv[])
                                 gpu_dbg("%d:      done @%p:%08x\n", i, h_done, ACCESS_ONCE(*h_done));
                                 gpu_dbg("%d: poll done @%p==%08x\n", i, h_done, value);
                                 gds_us_t tmout = 1000; //usecs
-                                int retcode = poll_dword_geq(h_done, value, tmout);
+                                retcode = poll_dword_geq(h_done, value, tmout);
                                 if (retcode == EWOULDBLOCK) {
-                                        // normal behaviour
+                                        // expected behaviour
                                         gpu_dbg("%d: unblocking stream by writing %p <= %08x\n", i, h_vals, 0);
                                         gds_atomic_set_dword(h_vals, 0);
                                         retcode = poll_dword_geq(h_done, value, tmout);
                                         if (retcode) {
-                                                gpu_err("got error %d, exiting\n", retcode);
-                                                exit(EXIT_FAILURE);
+                                                gpu_fail("error %d while polling done flag", retcode);
                                         }
                                 }
                                 else if (retcode) {
-                                        gpu_err("got error %d, exiting\n", retcode);
+                                        gpu_err("error %d while polling done\n", retcode);
                                         exit(EXIT_FAILURE);
                                 }
                                 else {
@@ -292,7 +317,8 @@ int main(int argc, char *argv[])
 
         if (n_errors) {
                 gpu_err("detected n_errors=%d\n", n_errors);
-        } else {
+        }
+        else {
                 //if (cuStreamQuery(gpu_stream) != CUDA_SUCCESS) {
                 //        gpu_err("stream must be idle at this point, iter:%d\n", i);
                 //        exit(EXIT_FAILURE);
