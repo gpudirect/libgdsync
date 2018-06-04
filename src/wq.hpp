6849f720f41473e2d89ddb3035eaa6cbb8fb916e
Davide Rossetti <davide.rossetti@gmail.com>
2018-06-04 15:00:50 -0700

    work queue bug fixes. unit test extended to cover multiple tasks and removal of task.
    - introduced task handle
    - modified queue & dequeue APIs
    - locking in worker thread was not properly protecting against dequeing, still racy

--- src/wq.hpp
+++ src/wq.hpp
@@ -1,3 +1,30 @@
+/* Copyright (c) 2018, NVIDIA CORPORATION. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *  * Neither the name of NVIDIA CORPORATION nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #pragma once
 
 #include <thread>
@@ -5,22 +32,33 @@
 #include <mutex>
 #include <list>
 #include <functional>
+#include <cstdio>
 
+#define WQDBG(FMT, ARGS...)   do { } while(0)
+//#define WQDBG(FMT, ARGS...)   do {                                    \
+//        printf("%s() " FMT "\n", __FUNCTION__ ,##ARGS);               \
+//    } while(0)
 
 // single threaded work queue
 struct work_queue {
     work_queue() {
+        WQDBG("CTOR");
         m_state = idle;
     }
 
     ~work_queue() {
+        WQDBG("DTOR");
         kill_worker();
     }
 
+    typedef std::function<bool()> task_t;
+    typedef std::list<task_t> task_list_t;
+    typedef task_list_t::iterator task_handle_t;
+
     template <typename Task>
-    void queue(Task t) {
+    task_handle_t queue(Task t) {
         lock_t lock(m_mtx);
-        m_tasks.push_back(t);
+        task_handle_t handle = m_tasks.insert(m_tasks.end(), t);
         if (m_state == idle) {
             m_worker = std::thread(std::bind(&work_queue::worker_thread, this));
             while (m_state == idle) {
@@ -28,37 +66,39 @@ struct work_queue {
             }
         }
         m_cond.notify_one();
+        return handle;
     }
 
-    template <typename Task>
-    void dequeue(Task &t) {
+    // WARNING: dequeuing can race with self-dequeing task
+    void dequeue(task_handle_t handle) {
+        WQDBG("dequing task");
         std::lock_guard<std::mutex> g(m_mtx);
-        m_tasks.remove(t);
+        m_tasks.erase(handle);
     }
 
 private:
     void kill_worker() {
-        puts("signaling worker thread");
+        WQDBG("killing worker thread");
         {
             lock_t g(m_mtx);
             if (m_state == running) {
-                puts("state<-draining");
+                WQDBG("setting queue state to draining");
                 m_state = draining;
                 m_cond.notify_one();
             }
         }
-        puts("joining");
+        WQDBG("joining worker thread");
         m_worker.join();
     }
 
     void worker_thread() {
         lock_t lock(m_mtx);
-        puts("state<-running");
+        WQDBG("state<-running");
         m_state = running;
         m_cond.notify_all();
         while(true) {
             if (m_state == draining) {
-                puts("state<-done");
+                WQDBG("state<-done");
                 m_state = done;
                 break;
             }
@@ -66,17 +106,17 @@ private:
                 m_cond.wait(lock);
             auto f = m_tasks.begin();
             while (f != m_tasks.end()) {
-                lock.unlock();
+                auto fnext = std::next(f);
                 task_t t = *f;
+                lock.unlock();
                 bool keep = t();
                 lock.lock();
                 if (!keep) {
-                    printf("removing task\n");
-                    f = m_tasks.erase(f);
-                    printf("done\n");
-                } else {
-                    ++f;
+                    WQDBG("removing task");
+                    m_tasks.erase(f);
+                    WQDBG("done");
                 }
+                f = fnext;
             }
         }
     }
@@ -87,10 +127,11 @@ private:
     std::thread m_worker;
     std::mutex m_mtx;
     typedef std::unique_lock<std::mutex> lock_t;
-    typedef std::function<bool()> task_t;
-    std::list<task_t> m_tasks;
+    task_list_t m_tasks;
     std::condition_variable m_cond;
 };
+
+#undef WQDBG
 
 /*
  * Local variables:
--- tests/wqtest.cpp
+++ tests/wqtest.cpp
@@ -1,31 +1,137 @@
+/* Copyright (c) 2016, NVIDIA CORPORATION. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *  * Neither the name of NVIDIA CORPORATION nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+// work around missing std::this_thread::sleep_until() in gcc-4.4.7 (RHL 6.x)
+#ifndef _GLIBCXX_USE_NANOSLEEP
+#define _GLIBCXX_USE_NANOSLEEP
+#endif
+
+#include <cstdio>
+//#include <cstdlib>
+#include <chrono>
+
 #include "wq.hpp"
-#include <unistd.h>
+
+#if 0
+#define msg(FMT, ARGS...)   do {                           \
+                printf("%s() " FMT, __FUNCTION__ ,##ARGS); \
+        } while(0)
+#else
+#define msg(FMT, ARGS...)   do { } while(0)
+#endif
 
 bool simple_fun()
 {
-    static int j = 0;
-    printf("%s j=%d\n", __FUNCTION__, j);
-    return ++j < 1000 ? true : false;
+        static int j = 0;
+        msg("%s j=%d\n", __FUNCTION__, j);
+        return ++j < 1000 ? true : false;
 }
 
-struct functor {
-    functor() : k(0) {}
+struct fast_printer {
+        fast_printer() : m_k(0) {}
 
-    bool run() {
-        printf("%s k=%d\n", __FUNCTION__, k);
-        return ++k < 200 ? true : false;
-    }
-    int k;
+        bool run() {
+                msg("%p fast_printer k=%d\n", this, m_k);
+                return ++m_k < 2000 ? true : false;
+        }
+        int m_k;
 };
+
+struct sticky_printer {
+        sticky_printer() : m_s(0) {}
+
+        bool run() {
+                msg("%p sticky_printer s=%d\n", this, m_s++);
+                return true;
+        }
+        int m_s;
+};
+
+struct slow_printer {
+        slow_printer(unsigned num_iterations=100) : m_i(0), m_ni(num_iterations), m_store_time(true) {}
+
+        bool run() {
+                auto now = std::chrono::system_clock::now();
+                if (m_store_time) {
+                        msg("storing now\n");
+                        m_last_time = now;
+                        m_store_time = false;
+                }
+        
+                if (now > m_last_time + std::chrono::milliseconds(1)) {
+                        m_last_time = now;
+                        msg("%p slow_printer i=%d\n", this, m_i);
+                        std::this_thread::sleep_for(std::chrono::microseconds(1));
+                        if ( ++m_i < m_ni ) {
+                                return true;
+                        } else {
+                                msg("slow_printer done!");
+                                return false;
+                        }
+                }
+        }
+        int m_i;
+        int m_ni;
+        bool m_store_time;
+        std::chrono::system_clock::time_point m_last_time;
+};
+
 
 main()
 {
-    work_queue wq;
-    wq.queue(&simple_fun);
-    functor f;
-    wq.queue(std::bind(&functor::run, &f));
-    sleep(1);
+        {
+                work_queue wq;
+                int i = 0;
+                
+                slow_printer s;
+                wq.queue(std::bind(&slow_printer::run, &s));
+
+                wq.queue(&simple_fun);
+                fast_printer f;
+                wq.queue(std::bind(&fast_printer::run, &f));
+
+                std::this_thread::sleep_for(std::chrono::microseconds(10));
+
+                do {
+                        sticky_printer sf;
+                        auto fp = std::bind(&sticky_printer::run, &sf);
+                        auto h = wq.queue(fp);
+                        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+                        wq.dequeue(h);
+                } while (++i<10);
+        }
+        fflush(stdout);
+        printf("SUCCESS\n");
 }
 
-
-// compile: c++ -g -std=c++0x wq.cpp -o wq
+/*
+ * Local variables:
+ *  c-indent-level: 8
+ *  c-basic-offset: 8
+ *  tab-width: 8
+ * End:
+ */
